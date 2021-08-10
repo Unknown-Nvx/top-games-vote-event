a93b45e0e69f6b07631dc2c034ff23e8c022b7b2
Unknown-Nvx <neveuxerwann@gmail.com>
2021-08-10 20:30:35 +0200

    add example command

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+module.exports = (bot, message, args) => {
+
+    console.log("help command loaded");
+    const Discord = require("discord.js");
+
+        const helpEmbed = new Discord.MessageEmbed()
+        .setColor('#5E6BDB')
+        .setAuthor('🔗 HELP')
+        .addField('👩‍🚀   $command1', '```Lorem ipsum dolor sit amet.```', false)
+        .addField('🔭   $command2', '```Lorem ipsum dolor sit amet.```', false)
+        .addField('🚀   $command3', '```Lorem ipsum dolor sit amet.```', false)
+        .addField('🛰️   $command4', '```Lorem ipsum dolor sit amet.```', false)
+        
+        message.channel.send(helpEmbed);
+
+}
