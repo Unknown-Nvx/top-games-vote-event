cfe6a5aa75d033f0e8dfad0b61c8ba9dfada9fdb
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2021-12-06 23:00:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Collection } = require("discord.js");
 const { prefix, token } = require("./config.json");
-const { Intents } = require('discord.js');
+const {Intents} = require('discord.js');
 const Discord = require("discord.js");
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -9,7 +9,7 @@ bot.commands = new Collection();
 
 bot.commands.set("help", require("./commands/help.js"));
 
-bot.on("ready", () => require("./events/ready.js")());
-bot.on("message", message => require("./events/message.js")(message));
+bot.on("ready", () => require("./events/ready.js")(bot));
+bot.on("message", message => require("./events/message.js")(bot, message));
 
 bot.login(process.env.TOKEN);
