00835f58ec72a5b5c65092b323dd259923c1c9c3
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2021-12-08 15:28:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"
 bot.prefix = prefix;
 bot.commands = new Collection();
 
-bot.commands.set("help", require("./commands/help.js"));
+bot.on('messageCreate', message => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
+});
+
+
+//bot.commands.set("help", require("./commands/help.js"));
 
 bot.on("ready", () => require("./events/ready.js")());
-bot.on("messageCreate", message => require("./events/message.js")(message));
+//bot.on("messageCreate", message => require("./events/message.js")(message));
 
 bot.login(process.env.TOKEN);
