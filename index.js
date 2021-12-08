30c95e59be022f0f1414746b8e28e8a38403602c
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2021-12-08 14:32:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 bot.prefix = prefix;
 bot.commands = new Collection();
 
-bot.commands.set("help", require("./commands/help.js"));
+//bot.commands.set("help", require("./commands/help.js"));
+
+bot.on('message', msg => {
+   if (msg.author.bot) return;
+   if (msg.content.startsWith(settings.prefix)) return;
+
+   if (msg.content.startsWith(settings.prefix + "ping")) {
+     msg.reply("Pong!") 
+   }
+});
 
 bot.on("ready", () => require("./events/ready.js")());
-bot.on("message", message => require("./events/message.js")(message));
+//bot.on("message", message => require("./events/message.js")(message));
 
 bot.login(process.env.TOKEN);
