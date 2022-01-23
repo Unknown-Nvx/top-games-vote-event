db11af40057e315dfafd0f3f3c37a1bccdf5b452
Unknown-Nvx <neveuxerwann@gmail.com>
2022-01-23 17:44:02 +0100

    first commit, create vote event

--- events/message.js
+++ events/message.js
@@ -1,10 +1,10 @@
 module.exports = (message, bot) => {
-      
+
       if (!message.content.startsWith(bot.prefix) || message.author.bot) return;
 
       const args = message.content.slice(bot.prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
 
-if(bot.commands.has(command)) bot.commands.get(command)(bot, message, args);
+      if (bot.commands.has(command)) bot.commands.get(command)(bot, message, args);
 
-     }
+}
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { Client, Intents, Collection } = require("discord.js");
 const { prefix, token } = require("./config.json");
 
-const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]});
+const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
 
 bot.prefix = prefix;
 bot.commands = new Collection();
 
 bot.on("ready", () => require("./events/ready.js")(bot));
 bot.on("messageCreate", (message) => require("./events/message.js")(message, bot));
+bot.on("messageCreate", (vote) => require("./events/vote.js")(vote, bot));
 
 bot.commands.set("help", require("./commands/help.js"));
 
-bot.login(process.env.TOKEN);
+bot.login(token);
