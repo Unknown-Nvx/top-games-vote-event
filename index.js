c2f63184e2d7dc0dc397d9808409b9e713aa88ff
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2022-01-28 15:12:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const { Client, Intents, Collection } = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { token } = require("./config.json");
 
-const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
-
-bot.prefix = prefix;
-bot.commands = new Collection();
-
-bot.on("ready", () => require("./events/ready.js")(bot));
-bot.on("messageCreate", (message) => require("./events/message.js")(message, bot));
+const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 bot.on("messageCreate", (vote) => require("./events/vote.js")(vote, bot));
-
-bot.commands.set("help", require("./commands/help.js"));
 
 bot.login(token);
