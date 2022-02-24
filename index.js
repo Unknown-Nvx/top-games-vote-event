8acf140ea85400b72d3aee4bbe1b0f5b8bbfc25f
Unknown8 <67058378+Unknown-Nvx@users.noreply.github.com>
2022-02-24 12:26:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const { Client, Intents, Collection } = require("discord.js");
 const { token } = require("./config.json");
 
 const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-bot.on("messageCreate", (vote) => require("./events/vote.js")(vote, bot));
+bot.on("messageCreate", (vote) => require("./vote.js")(vote, bot));
 
 bot.login(token);
